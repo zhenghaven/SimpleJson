b2c675e25d8f9c12d287612d7bdf48a5646eae4c
Haofan Zheng <hzheng6@ucsc.edu>
2021-04-15 06:34:14 +0000

    Renamed StaticSeq to ConstSeq in test code.

--- test/src/TestConstSequence.cpp
+++ test/src/TestConstSequence.cpp
@@ -13,27 +13,27 @@ namespace SimpleJson_Test
 	extern size_t g_numOfTestFile;
 }
 
-GTEST_TEST(TestStaticSequence, CountTestFile)
+GTEST_TEST(TestConstSequence, CountTestFile)
 {
 	++SimpleJson_Test::g_numOfTestFile;
 }
 
-GTEST_TEST(TestStaticSequence, UInt64Array)
+GTEST_TEST(TestConstSequence, UInt64Array)
 {
 	constexpr uint64_t testCArray[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 	std::array<uint64_t, sizeof(testCArray)/sizeof(uint64_t)> testArray;
 	std::copy(std::begin(testCArray), std::end(testCArray), testArray.begin());
 
-	static_assert(SIMPLEJSON_CONST_ARRAY(testCArray)::sk_size == 10, "StaticSequence Impl Error!");
+	static_assert(SIMPLEJSON_CONST_ARRAY(testCArray)::sk_size == 10, "ConstSequence Impl Error!");
 
 	auto outData = SIMPLEJSON_CONST_ARRAY(testCArray)::GetData();
 	EXPECT_EQ(outData, testArray);
 }
 
-GTEST_TEST(TestStaticSequence, String)
+GTEST_TEST(TestConstSequence, String)
 {
-	static_assert(SIMPLEJSON_CONST_STRING("Test_string_1")::sk_size == 13, "StaticSequence Impl Error!");
+	static_assert(SIMPLEJSON_CONST_STRING("Test_string_1")::sk_size == 13, "ConstSequence Impl Error!");
 
 	auto outStr = SIMPLEJSON_CONST_STRING("Test_string_1")::GetStr();
 	EXPECT_EQ(outStr, "Test_string_1");
